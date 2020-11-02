a4924a54d2acfaff29f48c39724285a8d2e34dce
Matt <matthieu.manuellan@gmail.com>
2020-11-02 13:52:53 +0100

    Début Rewired

---
+++ DodgeBallProject/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    [SerializeField]private int playerID = 0;
+    [SerializeField]private Player player;
+    void Start()
+    {
+        player = ReInput.players.GetPlayer(playerID);
+    }
+
+    void Update()
+    {
+        
+    }
+}

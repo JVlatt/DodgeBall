cb34c4687938807082deeae28ea3f6c2b4f61ee2
Matt <matthieu.manuellan@gmail.com>
2020-11-02 16:57:17 +0100

    Ball

---
+++ DodgeBallProject/Assets/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed = 2.0f;
+    public float multiplierFactor = 1.0f;
+    private Rigidbody _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void LateUpdate()
+    {
+        _rb.velocity = direction.normalized * speed * multiplierFactor;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            direction = Vector3.Reflect(direction, collision.contacts[0].normal);
+        }
+    }
+}

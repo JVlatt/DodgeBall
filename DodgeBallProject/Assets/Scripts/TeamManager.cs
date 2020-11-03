4e48d4a5f70fb21ba3f1af0059dc81ee3fb847be
Théo <theo.boldireff@edu.devinci.fr>
2020-11-03 18:10:47 +0100

    Team Selection

---
+++ DodgeBallProject/Assets/Scripts/TeamManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class TeamManager : MonoBehaviour
+{
+    [HideInInspector]
+    public List<TeamSelection> playerList;
+    [HideInInspector]
+    public bool launchGame;
+    [HideInInspector]
+    public int i = 0;
+
+    public List<int> blueTeam;
+    public List<int> redTeam;
+    private bool teamsCreated = false;
+
+    void Start()
+    {
+        launchGame = false;
+
+        var tmp = GameObject.Find("Controllers");
+
+        for(int i = 0; i < tmp.transform.childCount; i++)
+        {
+            if (tmp.transform.GetChild(i).gameObject.activeSelf)
+            {
+                playerList.Add(tmp.transform.GetChild(i).GetComponent<TeamSelection>());
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (!launchGame)
+        {
+            PlayerCheck();
+        }
+        else
+        {
+            if (!teamsCreated)
+            {
+                CreateTeams();
+                teamsCreated = true;
+            }
+        }
+    }
+
+    void PlayerCheck()
+    {
+        if (i < playerList.Count && playerList[i].isReady)
+        {
+            i++;
+        }
+
+        if(i == playerList.Count)
+        {
+            launchGame = true;
+        }
+    }
+
+    void CreateTeams()
+    {
+        for(int i = 0; i < playerList.Count; i++)
+        {
+            if(playerList[i].curTeam == TeamSelection.Team.Blue)
+            {
+                blueTeam.Add(playerList[i].playerID);
+            }
+
+            if(playerList[i].curTeam == TeamSelection.Team.Red)
+            {
+                redTeam.Add(playerList[i].playerID);
+            }
+        }
+    }
+}
---
+++ DodgeBallProject/Assets/Scripts/TeamSelection.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.UI;
+
+public class TeamSelection : MonoBehaviour
+{
+    public int playerID = 0;
+    [SerializeField] private Player player;
+
+    public GameObject playerCanvas;
+    [HideInInspector]
+    public GameObject pressToJoin;
+    [HideInInspector]
+    public GameObject controllerImage;
+    [HideInInspector]
+    public List<GameObject> cursorPosList;
+    [HideInInspector]
+    public bool hasJoin;
+    [HideInInspector]
+    public bool isReady;
+
+    public enum Team
+    {
+        None,
+        Red,
+        Blue,
+    }
+
+    public Team curTeam;
+
+    void Start()
+    {
+        player = ReInput.players.GetPlayer(playerID);
+        curTeam = Team.None;
+    }
+
+    void Update()
+    {
+        float cursorPos = player.GetAxis("Move");
+
+
+        if (!hasJoin && player.GetButtonDown("Join"))
+        {
+            pressToJoin = playerCanvas.transform.GetChild(0).gameObject;
+            pressToJoin.SetActive(false);
+            controllerImage = playerCanvas.transform.GetChild(1).gameObject;
+            controllerImage.SetActive(true);
+            hasJoin = true;
+
+            for(int i = 2; i < playerCanvas.transform.childCount; i++)
+            {
+                cursorPosList.Add(playerCanvas.transform.GetChild(i).gameObject);
+            }
+        }
+
+        if(hasJoin && player.GetButtonDown("Join") && controllerImage.transform.position != cursorPosList[1].transform.position)
+        {
+            isReady = true;
+
+            if(controllerImage.transform.position == cursorPosList[2].transform.position)
+            {
+                curTeam = Team.Blue;
+                controllerImage.GetComponent<Image>().color = Color.blue;
+            }
+
+            if (controllerImage.transform.position == cursorPosList[0].transform.position)
+            {
+                curTeam = Team.Red;
+                controllerImage.GetComponent<Image>().color = Color.red;
+            }
+        }
+
+        if (hasJoin && player.GetButtonDown("Leave"))
+        {
+            pressToJoin.SetActive(true);
+            controllerImage.SetActive(false);
+            controllerImage.transform.position = cursorPosList[1].transform.position;
+            controllerImage.GetComponent<Image>().color = Color.white;
+            cursorPosList.Clear();
+            hasJoin = false;
+            isReady = false;
+            curTeam = Team.None;
+        }
+
+        if (hasJoin && !isReady && cursorPos > 0)
+        {
+            controllerImage.transform.position = cursorPosList[2].transform.position;
+        }
+
+        if (hasJoin && !isReady && cursorPos < 0)
+        {
+            controllerImage.transform.position = cursorPosList[0].transform.position;
+        }
+    }
+}

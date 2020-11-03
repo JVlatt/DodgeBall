a25004d3fb6b990e8382ee1fb598983bb82e9450
Matt <matthieu.manuellan@gmail.com>
2020-11-03 20:06:45 +0100

    Rewind

--- DodgeBallProject/Assets/Scripts/Recorder.cs
+++ DodgeBallProject/Assets/Scripts/Recorder.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 using UnityEditor.Animations;
 
 public class Recorder : MonoBehaviour
 {
     public AnimationClip clip1;
-    public AnimationClip clip2;
+    public Text RecordTimer;
     private Animation m_player;
     public float clipDuration;
-    private float clipClock;
+    private float clipClock = 0f;
     private GameObjectRecorder m_Recorder;
     private bool isRecording = false;
     private bool recordA = true;
@@ -25,64 +26,128 @@ public class Recorder : MonoBehaviour
         m_Recorder.BindComponentsOfType<Transform>(gameObject, true);
     }
 
+    public void StartRecord()
+    {
+        System.Array.Clear(clip1.events, 0, clip1.events.Length);
+        recordA = true;
+        isRecording = true;
+        Debug.Log("Recording");
+    }
+
     void LateUpdate()
     {
         if (!isRecording)
             return;
 
-        m_Recorder.TakeSnapshot(Time.deltaTime);
         clipClock += Time.deltaTime;
-        if(clipClock >= clipDuration)
-        {
-            NextRecord();
-            clipClock = 0f;
-            recordA = !recordA;
-        }
-    }
-    public void NextRecord()
-    {
-        if (m_Recorder.isRecording)
-        {
-            if(recordA)
-            {
-                m_Recorder.SaveToClip(clip1);
-                if (clip1.length > clipDuration / 2)
-                {
-                    System.Array.Clear(clip2.events, 0, clip2.events.Length);
-                }
-                else
-                    doubleRecord = true;
-            }
-            else
-            {
-                m_Recorder.SaveToClip(clip2);
-                if (clip2.length > clipDuration / 2)
-                {
-                    System.Array.Clear(clip1.events, 0, clip2.events.Length);
-                }
-                else
-                    doubleRecord = true;
-            }
-            m_Recorder.ResetRecording();
-        }
-    }
-    
-    public void StopRecord()
-    {
-        isRecording = false;
-        NextRecord();
-    }
-    public void StartRecord()
-    {
-        System.Array.Clear(clip1.events, 0, clip1.events.Length);
-        System.Array.Clear(clip2.events, 0, clip2.events.Length);
-        isRecording = true;
+        RecordTimer.text = clipClock.ToString();
+        m_Recorder.TakeSnapshot(Time.deltaTime);
     }
 
+    public void StopRecord()
+    {
+        m_Recorder.SaveToClip(clip1);
+        isRecording = false;
+        Debug.Log("Stop Recording");
+    }
     public void PlayRecordClip()
     {
-        if(doubleRecord)
-        m_player.AddClip(clip1,"record1");
+        m_player.AddClip(clip1, "record1");
+        m_player["record1"].time = m_player["record1"].length - 5.0f;
         m_player.Play("record1");
     }
+    //void LateUpdate()
+    //{
+    //    if (!isRecording)
+    //        return;
+
+    //    RecordTimer.text = clipClock.ToString();
+    //    m_Recorder.TakeSnapshot(Time.deltaTime);
+    //    clipClock += Time.deltaTime;
+    //    if(clipClock >= clipDuration)
+    //    {
+    //        NextRecord();
+    //        clipClock = 0f;
+    //        recordA = !recordA;
+    //    }
+    //}
+    //public void NextRecord()
+    //{
+    //    Debug.Log("Recorder Status : " + m_Recorder.isRecording);
+    //    if (m_Recorder.isRecording)
+    //    {
+    //        if(recordA)
+    //        {
+    //            m_Recorder.SaveToClip(clip1);
+    //            if (clip1.length > clipDuration / 2)
+    //            {
+    //                System.Array.Clear(clip2.events, 0, clip2.events.Length);
+    //            }
+    //            else
+    //                doubleRecord = true;
+
+    //            Debug.Log("Clip 1 Duration : " + clip1.length);
+    //        }
+    //        else
+    //        {
+    //            m_Recorder.SaveToClip(clip2);
+    //            if (clip2.length > clipDuration / 2)
+    //            {
+    //                System.Array.Clear(clip1.events, 0, clip1.events.Length);
+    //            }
+    //            else
+    //                doubleRecord = true;
+
+    //            Debug.Log("Clip 2 Duration : " + clip2.length);
+    //        }
+    //        m_Recorder.ResetRecording();
+    //    }
+    //}
+
+    //public void StopRecord()
+    //{
+    //    NextRecord();
+    //    isRecording = false;
+    //    clipClock = 0f;
+    //    Debug.Log("Stop Recording");
+    //}
+    //public void StartRecord()
+    //{
+    //    System.Array.Clear(clip1.events, 0, clip1.events.Length);
+    //    System.Array.Clear(clip2.events, 0, clip2.events.Length);
+    //    recordA = true;
+    //    isRecording = true;
+    //    Debug.Log("Recording");
+    //}
+
+    //public void PlayRecordClip()
+    //{
+    //    if(doubleRecord)
+    //    {
+    //        if(recordA)
+    //        {
+    //            m_player.AddClip(clip2, "record1");
+    //            m_player.AddClip(clip1, "record2");
+    //        }
+    //        else
+    //        {
+    //            m_player.AddClip(clip1, "record1");
+    //            m_player.AddClip(clip2, "record2");
+    //        }
+
+    //        m_player.Play("record1");
+    //        m_player.PlayQueued("record2");
+
+    //        Debug.Log("Double Clip Duration : " + clip1.length + clip2.length);
+    //    }
+    //    else
+    //    {
+    //        if(recordA)
+    //            m_player.AddClip(clip1, "record1");
+    //        else
+    //            m_player.AddClip(clip2, "record1");
+
+    //        m_player.Play("record1");
+    //    }
+    //}
 }

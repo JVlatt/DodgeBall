d489ceb52d4a9d10fb82a7dff37480f3cecd90f7
Matt <matthieu.manuellan@gmail.com>
2020-11-05 00:15:55 +0100

    Video Recorder

--- DodgeBallProject/Assets/Scripts/Recorder.cs
+++ DodgeBallProject/Assets/Scripts/Recorder.cs
@@ -3,61 +3,31 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-/*
-using UnityEditor;
-using UnityEditor.Animations;
-*/
+using RockVR.Video;
 public class Recorder : MonoBehaviour
 {
-    /*
-    private AnimationClip clip1;
-    private Animation m_player;
-    public float clipDuration;
     private float clipClock = 0f;
-    public GameObjectRecorder m_Recorder;
     private bool isRecording = false;
     private bool recordA = true;
     private bool doubleRecord = false;
-    void Awake()
+    
+    public void Awake()
     {
-        // Create recorder and record the script GameObject.
-        clip1 = new AnimationClip();
-        clip1.legacy = true;
-        m_Recorder = new GameObjectRecorder(gameObject);
-        m_player = GetComponent<Animation>();
-        // Bind all the Transforms on the GameObject and all its children.
-        m_Recorder.BindComponentsOfType<Transform>(gameObject, true);
+        StartRecord();
     }
-
     public void StartRecord()
     {
-        System.Array.Clear(clip1.events, 0, clip1.events.Length);
-        recordA = true;
-        isRecording = true;
-        Debug.Log("Recording");
+        VideoCaptureCtrl.instance.StartCapture();
     }
 
     void LateUpdate()
     {
-        if (!isRecording)
-            return;
-
-        clipClock += Time.deltaTime;
-        m_Recorder.TakeSnapshot(Time.deltaTime);
     }
 
     public void StopRecord()
     {
-        m_Recorder.SaveToClip(clip1);
-        isRecording = false;
-        Debug.Log("Stop Recording");
     }
     public void PlayRecordClip()
     {
-        Debug.Log("Clip Duration : " + clip1.length);
-        m_player.AddClip(clip1, "record1");
-        m_player["record1"].time = m_player["record1"].length - 5.0f;
-        m_player.Play("record1");
     }
-    */
 }

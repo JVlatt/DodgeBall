44ea377af1d10e3c8173ffa529a9302173efef6e
Théo <theo.boldireff@edu.devinci.fr>
2020-11-05 16:39:57 +0100

    Sound manager

---
+++ DodgeBallProject/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [Header("General Sounds")]
+    public AudioSource main_theme;
+    public AudioSource button_click;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        instance.MainTheme();
+    }
+
+    //General
+    public void MainTheme()
+    {
+        main_theme.Play();
+    }
+
+    public void ButtonClick()
+    {
+        button_click.Play();
+    }
+}

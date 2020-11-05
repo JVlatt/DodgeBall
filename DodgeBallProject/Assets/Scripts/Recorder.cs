65fa3f08404d005e3b3f9f29f1ccfe9e95b6aedc
Matt <matthieu.manuellan@gmail.com>
2020-11-05 04:23:22 +0100

    Pre-Commit

--- DodgeBallProject/Assets/Scripts/Recorder.cs
+++ DodgeBallProject/Assets/Scripts/Recorder.cs
@@ -3,31 +3,94 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-using RockVR.Video;
 public class Recorder : MonoBehaviour
 {
-    private float clipClock = 0f;
-    private bool isRecording = false;
-    private bool recordA = true;
-    private bool doubleRecord = false;
-    
-    public void Awake()
+    [Serializable]
+    public class RecordInfo
     {
-        StartRecord();
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public RecordInfo(Vector3 _position, Quaternion _rotation)
+        {
+            position = _position;
+            rotation = _rotation;
+        }
+    }
+    public Dictionary<int, List<RecordInfo>> recordInfos;
+    public List<GameObject> objectsToRecord = new List<GameObject>(1);
+    public float clipDuration = 20f;
+    private bool isRecording = false;
+    private bool isPlayingClip = false;
+
+    void FixedUpdate()
+    {
+        if (isRecording)
+        {
+            Record();
+        }
+        else if (isPlayingClip)
+            Play();
+    }
+
+    public void Play()
+    {
+        for (int i = 0; i < objectsToRecord.Count - 1; i++)
+        {
+                if (recordInfos[i].Count > 0)
+                {
+                    Debug.Log(recordInfos[i].Count);
+                    objectsToRecord[i].transform.position = recordInfos[i][recordInfos[i].Count - 1].position;
+                    objectsToRecord[i].transform.rotation = recordInfos[i][recordInfos[i].Count - 1].rotation;
+                    recordInfos[i].RemoveAt(recordInfos[i].Count - 1);
+                }
+                else
+                    StopRecordClip();
+        }
+    }
+
+    public void Record()
+    {
+        for (int i = 0; i < objectsToRecord.Count - 1; i++)
+        {
+            if (recordInfos[i].Count > Mathf.Round(clipDuration / Time.fixedDeltaTime))
+            {
+                recordInfos[i].RemoveAt(recordInfos[i].Count - 1);
+            }
+            recordInfos[i].Insert(0, new RecordInfo(objectsToRecord[i].transform.position, objectsToRecord[i].transform.rotation));
+        }
     }
     public void StartRecord()
     {
-        VideoCaptureCtrl.instance.StartCapture();
+        recordInfos = new Dictionary<int, List<RecordInfo>>();
+        for (int i = 0; i < objectsToRecord.Count - 1; i++)
+        {
+            recordInfos[i] = new List<RecordInfo>();
+        }
+        isRecording = true;
+    }
+    public void AddObjectToRecord(GameObject objectToAdd)
+    {
+        objectsToRecord.Add(objectToAdd);
+        recordInfos[objectsToRecord.Count - 1] = new List<RecordInfo>();
     }
 
-    void LateUpdate()
+    public void RemoveObjectToRecord(GameObject objectToRemove)
     {
+        recordInfos.Remove(objectsToRecord.IndexOf(objectToRemove));
+        objectsToRecord.Remove(objectToRemove);
     }
 
     public void StopRecord()
     {
+        isRecording = false;
+    }
+    public void StopRecordClip()
+    {
+        isPlayingClip = false;
     }
     public void PlayRecordClip()
     {
+        isPlayingClip = true;
     }
 }
